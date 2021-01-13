b3654a9d199ee5f99d6041813c4210d3a7554a84
mlima3696 <mjuniorlima3@gmail.com>
2021-01-12 21:42:53 -0300

    PUT /orders/{id}/delivered

--- backend/src/main/java/com/devsuperior/dsdeliver/controllers/OrderControllers.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/controllers/OrderControllers.java
@@ -6,11 +6,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.devsuperior.dsdeliver.dto.OrderDTO;
@@ -36,6 +37,11 @@ public class OrderControllers {
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
 				.buildAndExpand(dto.getId()).toUri();
 		return ResponseEntity.created(uri).body(dto);
+	}
+	@PutMapping("/{id}/delivered")
+	public ResponseEntity<OrderDTO> setDelivered(@PathVariable Long id){
+		OrderDTO dto = service.setDelivered(id);
+		return ResponseEntity.ok().body(dto);
 	}
 	
 }
